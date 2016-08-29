70c53108aa410712500399816a685eff73068960
Kyle Robertson <sx3wiz@gmail.com>
2016-08-29 17:33:02 -0500

    Initial structure and documentation

---
+++ aes128key.h
@@ -0,0 +1,41 @@
+#ifndef AES128KEY
+#define AES128KEY
+
+#include <vector>
+#include <string>
+#include <unistd.h>
+
+class AES128Key {
+public:
+	/**
+		Constructs the object with existing key data.
+
+		@param[key_data] A 32 byte long vector<uint8_t> containing key data.
+	*/
+	AES128Key(vector<uint8_t>* key_data);
+
+	/**
+		Constructs the object with existing key data in hex string form.
+
+		@param[key_data_as_hex] A hex string of 32 characters containing key data.
+	*/
+	AES128Key(std::string key_data_as_hex);
+
+	/**
+		An overload of the [] operator allow convenient access to key data.
+
+		@return Returns the byte of key data at the requested reference.
+	*/
+	uint8_t& operator[](int x) const;
+
+	/**
+		Converts the key data to a hex string
+
+		@return A hex string of the key data 32 characters in length.
+	*/
+	std::string toHexString() const;
+protected:
+	vector<uint8_t> *m_key_data;
+};
+
+#endif
---
+++ mstnfc.h
@@ -0,0 +1,85 @@
+#ifndef MSTNFC_H
+#define MSTNFC_H
+
+#include <unistd.h>
+#include "aes128key.h"
+
+class MSTNFC {
+public:
+	MSTNFC();
+	/**
+		Initializes the NFC reader and prepares it to scan cards.
+		@return Returns true if initialization occurred successfully otherwise false
+	*/
+	bool init();
+
+	/**
+		Attempts to create an application on the connected card.
+
+		@param[aid] The Application ID stored in the last 3 bytes of the integer
+			0x00XXXXXX as depicted by X's
+		@return Returns true if success and false on an error.
+	*/
+	bool createApplication(uint32_t aid);
+
+	/**
+		Lists the applications currently on the card.
+
+		@return A vector containing the application IDs on the card. Each value stores an
+			AID in the last 3 bytes of an integer, 0x00XXXXXX demonstrated using X's. Returns
+			null if failure.
+	*/
+	vector<uint32_t>* listApplications() const;
+
+	/**
+		Checks if the card contains a specific Application by ID.
+
+		@param[aid] The application ID stored as the last 3 bytes of a integer, 0x00XXXXXX
+			 demonstrated using X's.
+		@return Returns true on sucess and false on failure.
+	*/
+	bool containsApplication(uint32_t aid) const;
+
+	/**
+		Attempt to get the UID of the connected card.
+
+		@return A vector<uint8_t> containing the bytes of the UID. Returns null if failure.
+	*/
+	vector<uint8_t>* getUID() const;
+
+	/**
+		Function returns status of connected card.
+
+		@return Returns true if card is currently connected and false if not.
+	*/
+	bool isCardPresent() const;
+
+	/**
+		Initiates a scan for cards.
+
+		@return Returns true if scan was finished successfully and false if there was an error.
+	*/
+	bool scanForCards() const;
+
+	/**
+		Attempts to authenticate to an application on connected card using an AES key.
+
+		@param[key] An AES128Key object containing the key data for the master key for the AID.
+		@param[aid] The application ID stored in the last 3 bytes of an integer 0x00XXXXXX like so.
+		@return True on success and false on failure.
+	*/
+	bool authenticate(AES128Key* key, uint32_t aid) const;
+
+	/**
+		Generates three random keys and stores them in the specified applicaiton.
+
+		@param[old_key_data] The key data for the old master key of the application.
+		@param[aid] The application ID stored as the last 3 bytes of a integer, 0x00XXXXXX
+			 demonstrated using X's.
+		@return Returns the 3 keys as AES128Key objects stored in a vector<AES128Key>.
+	*/
+	vector<AES128Key>* generateKeys(AES128Key* old_key_data, uint32_t aid);
+protected:
+};
+
+#endif
